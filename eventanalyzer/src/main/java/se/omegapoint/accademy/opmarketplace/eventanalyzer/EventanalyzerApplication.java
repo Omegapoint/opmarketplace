b6fadbbfac0082aa6ca430dbf1db894cbbb1a47f
Johan Arnör <johan.arnor@gmail.com>
2016-02-25 14:59:46 +0100

    Added project for the event analyzer.

---
+++ eventanalyzer/src/main/java/se/omegapoint/accademy/opmarketplace/eventanalyzer/EventanalyzerApplication.java
@@ -0,0 +1,12 @@
+package se.omegapoint.accademy.opmarketplace.eventanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventanalyzerApplication.class, args);
+	}
+}
---
+++ eventanalyzer/src/test/java/se/omegapoint/accademy/opmarketplace/eventanalyzer/EventanalyzerApplicationTests.java
@@ -0,0 +1,18 @@
+package se.omegapoint.accademy.opmarketplace.eventanalyzer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EventanalyzerApplication.class)
+@WebAppConfiguration
+public class EventanalyzerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
