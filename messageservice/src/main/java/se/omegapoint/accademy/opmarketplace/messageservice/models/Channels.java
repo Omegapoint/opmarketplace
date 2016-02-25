a68663204fc4ea6b4cdf6cd247b4a889aafb3412
Johan Arnör <johan.arnor@gmail.com>
2016-02-25 14:35:39 +0100

    Bug fix for the regex for all channels.

--- messageservice/src/main/java/se/omegapoint/accademy/opmarketplace/messageservice/models/Channels.java
+++ messageservice/src/main/java/se/omegapoint/accademy/opmarketplace/messageservice/models/Channels.java
@@ -4,7 +4,7 @@ package se.omegapoint.accademy.opmarketplace.messageservice.models;
 public final class Channels {
     public static final String RULE_COMMAND = "rule_command";
     public static final String EVENTS = "events";
-    public static final String ALL_CHANNELS_REGEX = "\\w"; //TODO: Includes RULE_COMMAND channel atm.
+    public static final String ALL_CHANNELS_REGEX = "\\w+"; //TODO: Includes RULE_COMMAND channel atm.
 
     private Channels() {}
 }
