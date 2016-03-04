a76b0d8f43931564c1d3b3ddb7570df675fc5283
Jonas Stendahl <jonas.stendahl@outlook.com>
2016-03-04 16:14:05 +0100

    Items

---
+++ frontend/app/item.component.ts
@@ -0,0 +1,24 @@
+import {Component, View} from "angular2/core";
+import {OnInit} from "angular2/core";
+
+export class Item{
+    constructor(private title: string, private description: string, private price: number, private expiration: Date){}
+}
+
+@Component({
+    selector: 'item',
+    inputs: ['item'],
+})
+
+@View({
+    templateUrl: 'app/item.html',
+})
+
+export class ItemComponent implements OnInit{
+    private item: Item;
+    private expireString: string;
+
+    public ngOnInit(){
+        this.expireString = this.item.expiration.toDateString();
+    }
+}
--- frontend/app/marketplace.component.ts
+++ frontend/app/marketplace.component.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {View} from "angular2/core";
 import 'rxjs/add/operator/map';
+import {Item} from "./item.component";
+import {ItemComponent} from "./item.component";
 
 @Component({
     selector: 'marketplace',
@@ -8,8 +10,21 @@ import 'rxjs/add/operator/map';
 
 @View({
     templateUrl: '/app/marketplace.html',
+    directives: [ItemComponent],
 })
 
 export class MarketplaceComponent {
     private query: string;
+    private items = ITEMS;
 }
+
+var ITEMS: Item[] = [
+    new Item("Item1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 100, new Date()),
+    new Item("Item2", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 200, new Date()),
+    new Item("Item3", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 300, new Date()),
+    new Item("Item4", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 4000, new Date()),
+    new Item("Item5", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 500, new Date()),
+    new Item("Item6", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 600, new Date()),
+    new Item("Item7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 7000, new Date()),
+    new Item("Item8", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tortor eget tincidunt ultrices. Sed. ", 8000, new Date()),
+];
