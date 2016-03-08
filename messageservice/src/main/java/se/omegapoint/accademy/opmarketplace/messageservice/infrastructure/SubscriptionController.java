a6255c99da91ed02a4116de19024a6ce492f9605
Johan Arnör <johan.arnor@gmail.com>
2016-03-08 12:59:14 +0100

    Refactored the subscriptions into bean.

---
+++ messageservice/src/main/java/se/omegapoint/accademy/opmarketplace/messageservice/infrastructure/SubscriptionController.java
@@ -0,0 +1,40 @@
+package se.omegapoint.accademy.opmarketplace.messageservice.infrastructure;
+
+import reactor.bus.EventBus;
+import reactor.bus.selector.Selector;
+import se.omegapoint.accademy.opmarketplace.messageservice.application.EventDispatcher;
+import se.omegapoint.accademy.opmarketplace.messageservice.domain.RuleEngine;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class SubscriptionController {
+    private final EventBus eventBus;
+    private final RuleEngine ruleEngine;
+
+    private HashMap<String, EventDispatcher> subscribedEndpoints = new HashMap<>();
+    private HashMap<String, HashSet<String>> subscriptions = new HashMap<>();
+
+    public SubscriptionController(EventBus eventBus, RuleEngine ruleEngine) {
+        this.eventBus = eventBus;
+        this.ruleEngine = ruleEngine;
+    }
+
+    public void subscribeEndpoint(String endpoint, Selector selector) {
+        if (!subscribedEndpoints.containsKey(endpoint)) {
+            subscribedEndpoints.put(endpoint, new EventDispatcher(ruleEngine, endpoint));
+        }
+
+        if (subscriptions.get(endpoint) == null)
+            subscriptions.put(endpoint, new HashSet<>());
+
+        // Check if the endpoint is already subscribed to the channel
+        if (!subscriptions.get(endpoint).contains(selector.getObject().toString())) {
+            subscriptions.get(endpoint).add(selector.getObject().toString());
+            eventBus.on(selector, subscribedEndpoints.get(endpoint));
+            System.out.printf("Subscribed endpoint %s to channel %s%n", endpoint, selector.getObject().toString());
+        } else {
+            System.out.printf("Endpoint %s is already subscribed to channel %s%n", endpoint, selector.getObject().toString());
+        }
+    }
+}
