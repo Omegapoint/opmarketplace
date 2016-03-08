3f797d05c9e4f0c0d44e35a59e32923831f055f3
Johan Arnör <johan.arnor@gmail.com>
2016-03-08 09:41:03 +0100

    Adjusted RemoteEvent for Jackson.

--- customer/src/main/java/se/omegapoint/academy/opmarketplace/customer/infrastructure/json_representations/RemoteEvent.java
+++ customer/src/main/java/se/omegapoint/academy/opmarketplace/customer/infrastructure/json_representations/RemoteEvent.java
@@ -3,10 +3,18 @@ package se.omegapoint.academy.opmarketplace.customer.infrastructure.json_represe
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.sql.Timestamp;
+
 public class RemoteEvent {
 
     private String type;
     private String data;
+    private Timestamp timestamp;
+
+    // TODO: 08/03/16 Fix factory method.
+//    public static RemoteEvent fromJsonModel(JsonModel data, String type) {
+//
+//    }
 
     public RemoteEvent(JsonModel data, String type){
         this.type = type;
@@ -17,11 +25,18 @@ public class RemoteEvent {
         }
     }
 
+    // Empty constructor and getters because of Jackson.
+    public RemoteEvent() {}
+
     public String getType() {
         return type;
     }
 
     public String getData() {
         return data;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 }
