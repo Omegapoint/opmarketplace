4b16eafd407fc5e9d1cf38c9db915e28e9e0dd5f
Jonas Stendahl <jonas.stendahl@outlook.com>
2016-03-13 11:32:32 +0100

    Added publisher url to properties

--- apigateway/src/main/java/se/omegapoint/academy/opmarketplace/apigateway/infrastructure/EventRemotePublisherService.java
+++ apigateway/src/main/java/se/omegapoint/academy/opmarketplace/apigateway/infrastructure/EventRemotePublisherService.java
@@ -2,17 +2,28 @@ package se.omegapoint.academy.opmarketplace.apigateway.infrastructure;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.springframework.beans.factory.annotation.Value;
 import se.omegapoint.academy.opmarketplace.apigateway.infrastructure.json_representations.RemoteEvent;
 
 import java.io.UnsupportedEncodingException;
 
+import static se.sawano.java.commons.lang.validate.Validate.notNull;
+
 
 public class EventRemotePublisherService {
-    CloseableHttpAsyncClient httpclient;
+
+    private static final FutureCallback<HttpResponse> IGNORE_CALLBACK = null;
+
+    @Value("${event.publisher.url}")
+    private String PUBLISH_URL;
+
+    private final CloseableHttpAsyncClient httpclient;
 
     public EventRemotePublisherService(){
         httpclient = HttpAsyncClients.createDefault();
@@ -20,21 +31,15 @@ public class EventRemotePublisherService {
     }
 
     public void publish(RemoteEvent remoteEvent) {
+        notNull(remoteEvent);
         try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        StringEntity eventJson = null;
-        try {
-            eventJson = new StringEntity(new ObjectMapper().writeValueAsString(remoteEvent));
+            StringEntity eventJson = new StringEntity(new ObjectMapper().writeValueAsString(remoteEvent));
+            HttpPost httpPost = new HttpPost(PUBLISH_URL + "?channel=Account");
+            httpPost.addHeader("Content-Type", "application/json");
+            httpPost.setEntity(eventJson);
+            httpclient.execute(httpPost, IGNORE_CALLBACK);
         } catch (UnsupportedEncodingException | JsonProcessingException e) {
             e.printStackTrace();
         }
-        HttpPost httpPost = new HttpPost("http://localhost:8000/event?channel=Account");
-        httpPost.addHeader("Content-Type", "application/json");
-        httpPost.setEntity(eventJson);
-
-        httpclient.execute(httpPost, null);
     }
 }
