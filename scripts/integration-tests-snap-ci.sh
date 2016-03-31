edb9989e91d7189659f5a6d1eb8b4b14dd2e87ec
Jonas Stendahl <jonas.stendahl@outlook.com>
2016-03-31 13:56:13 +0200

    snap-ci integration test script which use artifacts created in previous stages

---
+++ scripts/integration-tests-snap-ci.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ev
+# messageservice has to be first in array to enable
+# other services to subscribe on startup!
+DIRS=(messageservice customer marketplace)
+PIDS=()
+for DIR in ${DIRS[@]}; do
+  echo -------------------------Starting build of $DIR-------------------------
+  cd ../$DIR
+  java -jar build/libs/$DIR-0.0.1-SNAPSHOT.jar &
+  PIDS+=($!)
+  cd ../scripts
+done
+
+echo -------------------------Starting build of apigateway-------------------------
+cd ../apigateway
+chmod +x gradlew
+./gradlew integrationTest
+cd ../scripts
+
+echo -------------------------CleanUp-------------------------
+for PID in ${PIDS[@]}; do
+  echo Killing process: $PID...
+  kill $PID
+done
