4dd95d96ab39294a2a2f1887cf3277bdae76fce5
Johan Arnör <johan.arnor@gmail.com>
2016-04-08 12:13:19 +0200

    Added some tests for RuleEngine.

--- apigateway/src/test/java/se/omegapoint/academy/opmarketplace/apigateway/application/RuleEngineTest.java
+++ apigateway/src/test/java/se/omegapoint/academy/opmarketplace/apigateway/application/RuleEngineTest.java
@@ -2,7 +2,10 @@ package se.omegapoint.academy.opmarketplace.apigateway.application;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class RuleEngineTest {
 
@@ -28,4 +31,25 @@ public class RuleEngineTest {
         Thread.sleep(1500);
         assertTrue(ruleEngine.shouldAllowEvent(eventName));
     }
+
+    @Test
+    public void should_allow_user() throws Exception {
+        String email = "test@email.com";
+        assertTrue(ruleEngine.shouldAllowUser(email));
+    }
+
+    @Test
+    public void should_allow_user_who_is_on_the_list() throws Exception {
+        String email = "test@email.com";
+        ruleEngine.allowUsers(Arrays.asList(email), 1);
+        assertTrue(ruleEngine.shouldAllowUser(email));
+    }
+
+    @Test
+    public void should_deny_user_who_is_not_on_the_list() throws Exception {
+        String email = "test@email.com";
+        ruleEngine.allowUsers(Arrays.asList(email), 1);
+        assertFalse(ruleEngine.shouldAllowUser("not_test@email.com"));
+    }
+
 }
